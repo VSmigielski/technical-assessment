b6138b6f2dfedc62ae7d59b73813e5e33eb9f00c
Veronica <veronicasmigielski@gmail.com>
2021-06-22 12:32:28 +0200

    Updated

--- src/Components/Main/Main.js
+++ src/Components/Main/Main.js
@@ -1,16 +1,10 @@
 import React from 'react'
-import { Container, useMediaQuery, useTheme } from '@material-ui/core'
-import Desktop from './Desktop'
-import Tablet from './Tablet'
+import { Container } from '@material-ui/core'
 
 const Main = () => {
-    const theme = useTheme();
-
-    const isMedium = useMediaQuery(theme.breakpoints.down('md'));
 
     return (
         <Container>
-        {isMedium ? <Tablet></Tablet> : <Desktop></Desktop>}
         </Container>
     );
 }
